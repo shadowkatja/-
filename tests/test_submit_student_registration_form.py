dbd049f40880d5b7325cd54db5cb7d6e99268304
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-17 16:40:19 +0300

    refactor code to PageObj with mid level steps

--- tests/test_submit_student_registration_form.py
+++ tests/test_submit_student_registration_form.py
@@ -3,47 +3,31 @@ from selene import command
 from selene.support.shared import browser
 
 import tests_demo.recourses
+from tests_demo.pages.registration_page import RegistrationPage
 
 
-def test_submit_student_registration_form():
-    # open url
-    browser.open('/automation-practice-form')
-    browser.execute_script('document.querySelector(".body-height").style.transform = "scale(.75)"')
-    # Fill name
-    browser.element('#firstName').type('Charles')
-    browser.element('#lastName').type('Leclerc')
-    # Fill email
-    browser.element('#userEmail').type('CL16@test.com')
-    # Fill gender
-    browser.element('[for="gender-radio-1"]').click()
-    # fill phone number
-    browser.element('#userNumber').type('7900900909')
-    # fill birthdate
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__month-select').send_keys('October')
-    browser.element('.react-datepicker__year-select').click().send_keys('1997')
-    browser.element(f'.react-datepicker__day--0{16}').click()
-    # fill subjects
-    browser.element('#subjectsInput').click().type('arts').press_enter()
-    # fill hobbies
-    browser.all('.custom-control').element_by(have.exact_text("Sports")).click()
-    # scrolling to submit button
-    browser.element('#submit').perform(command.js.scroll_into_view)
-    # add picture
-    browser.element('#uploadPicture').send_keys(tests_demo.recourses.path('image.png'))
-    # add address
-    browser.element('#currentAddress').type('Monaco, Avenue de la Costa')
-    # add state and city
-    browser.element('[id="state"]').click()
-    browser.all('[id^="react-select"][id*=option]').element_by(have.exact_text('Haryana')).click()
-    browser.element('[id="city"]').click()
-    browser.all('[id^="react-select"][id*=option]').element_by(have.exact_text('Panipat')).click()
-    # press submit
-    browser.element('#submit').click()
+def test_submit_student_registration_form_by_mid_steps():
+
+    registration_page = RegistrationPage()
+
+    registration_page.open_form()
+    registration_page.fill_first_name('Charles')
+    registration_page.fill_last_name('Leclerc')
+    registration_page.fill_email('CL16@test.com')
+    registration_page.fill_gender()
+    registration_page.fill_number('7900900909')
+    registration_page.fill_date_of_birth(year='1997', month='October', day='16')
+    registration_page.fill_subjects('Arts')
+    registration_page.fill_hobbies('Sports')
+    registration_page.scroll_form()
+    registration_page.add_image('image.png')
+    registration_page.fill_address('Monaco, Avenue de la Costa')
+    registration_page.fill_state('Haryana')
+    registration_page.fill_city('Panipat')
+    registration_page.submit_form()
 
     # checking
-    browser.element('.table').all('tr td:nth-child(2)').should(have.texts
-        (
+    registration_page.should_have_registrated_user_with(
         'Charles Leclerc',
         'CL16@test.com',
         'Male',
@@ -54,4 +38,4 @@ def test_submit_student_registration_form():
         'image.png',
         'Monaco, Avenue de la Costa',
         'Haryana Panipat'
-    ))
+    )
