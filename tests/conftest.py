0e408f8437f4961ebfd06ead8671cf6be23e3e5b
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-17 16:39:08 +0300

    add config window

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,8 @@ from selene.support.shared import browser
 def set_browser():
     browser.config.base_url = 'https://demoqa.com'
     browser.config.timeout = 10
-    #browser.config.window_width = 1280
-    #browser.config.window_height = 1024
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
 
     yield
 
